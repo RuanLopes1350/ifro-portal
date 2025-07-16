fe7a95cc97138cd71c1c2c9d1f16eaa379cc41ab
RuanLopes1350 <romeu.unidac.alpha.nautilos@gmail.com>
2025-07-16 00:24:28 -0400

    div numeros feito

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import './styles/style.css'
 import './styles/header.css'
 import './styles/cards.css'
 import './styles/map.css'
+import './styles/number.css'
 
+// import da função para o mapa interativo
 import { setupMapInteraction } from './interatividade-mapa';
 
+// Inicializa a interatividade do mapa
 setupMapInteraction();
