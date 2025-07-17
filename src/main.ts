2e31c764e93869ad9b6631e713958c520674d55c
RuanLopes1350 <romeu.unidac.alpha.nautilos@gmail.com>
2025-07-16 21:02:07 -0400

    quase acabando

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import './styles/cards.css'
 import './styles/map.css'
 import './styles/number.css'
 import './styles/evento.css'
+import './styles/videos.css'
+import './styles/footer.css'
 
 // import da função para o mapa interativo
 import { setupMapInteraction } from './interatividade-mapa';
